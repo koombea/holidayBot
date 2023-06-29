c41eaae6023ccd615fc243a38b735525b4a8c689
Gal Gordon <gal.gordon@gmail.com>
2023-06-29 15:40:31 -0300

    added bolt

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require('express');
 require('dotenv').config();
-const path = require('path');
 
-const app = express();
-const port = process.env.PORT || 8080;
+const { App } = require('@slack/bolt');
 
-// sendFile will go here
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
